a8a1d90e3107b6c38ba5f0ab2db09db6a779badf
premcool <prem@prems.in>
2024-10-21 09:04:52 +0530

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use an official Python runtime as a parent image
+FROM python:3.9
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Install any needed packages specified in requirements.txt
+RUN pip3 install --no-cache-dir -r requirements.txt
+
+# Make port 8501 available to the world outside this container
+EXPOSE 8501
+
+# Define environment variable
+ENV GOOGLE_API_KEY='AIzaSyAJnbLrzj7fQtH8Qd3UMZQGbIkHpa3UAaU'
+
+# Run app.py when the container launches
+CMD ["streamlit", "run", "main.py"]
---
+++ main.py
@@ -0,0 +1,85 @@
+import os
+import streamlit as st
+import pickle
+import time
+import langchain_google_genai
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+
+from langchain_community.embeddings import FakeEmbeddings
+#from langchain_google_genai import GeminiEmbeddings
+
+
+from dotenv import load_dotenv
+load_dotenv()  # take environment variables from .env (especially openai api key)
+
+os.environ[ 'GOOGLE_API_KEY' ] = 'AIzaSyAJnbLrzj7fQtH8Qd3UMZQGbIkHpa3UAaU' #for Gemini
+
+st.title("Prem-Bot: News Research Tool 📈")
+st.sidebar.title("News Article URLs")
+
+urls = []
+for i in range(3):
+    url = st.sidebar.text_input(f"URL {i+1}")
+    urls.append(url)
+
+process_url_clicked = st.sidebar.button("Process URLs")
+file_path = "faiss_store_openai.pkl"
+
+main_placeholder = st.empty()
+#llm = OpenAI(temperature=0.9, max_tokens=500)
+llm = langchain_google_genai.ChatGoogleGenerativeAI(model="gemini-pro")
+
+if process_url_clicked:
+    # load data
+    loader = UnstructuredURLLoader(urls=urls)
+    main_placeholder.text("Data Loading...Started...✅✅✅")
+    data = loader.load()
+    # split data
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=['\n\n', '\n', '.', ','],
+        chunk_size=1000
+    )
+    main_placeholder.text("Text Splitter...Started...✅✅✅")
+    docs = text_splitter.split_documents(data)
+    # create embeddings and save it to FAISS index
+    # embeddings = OpenAIEmbeddings()
+    # Initialize langchain fake embeddings
+    embeddings = FakeEmbeddings(size=1352)
+
+    #vectorstore_openai = FAISS.from_documents(docs, embeddings)
+    # Create a FAISS vector store from documents using Google Gemini embeddings
+    vectorstore_openai = FAISS.from_documents(docs, embeddings)
+
+    main_placeholder.text("Embedding Vector Started Building...✅✅✅")
+    time.sleep(2)
+
+    # Save the FAISS index to a pickle file
+    with open(file_path, "wb") as f:
+        pickle.dump(vectorstore_openai, f)
+
+query = main_placeholder.text_input("Question: ")
+if query:
+    if os.path.exists(file_path):
+        with open(file_path, "rb") as f:
+            vectorstore = pickle.load(f)
+            chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
+            result = chain({"question": query}, return_only_outputs=True)
+            # result will be a dictionary of this format --> {"answer": "", "sources": [] }
+            st.header("Answer")
+            st.write(result["answer"])
+
+            # Display sources, if available
+            sources = result.get("sources", "")
+            if sources:
+                st.subheader("Sources:")
+                sources_list = sources.split("\n")  # Split the sources by newline
+                for source in sources_list:
+                    st.write(source)
+
+
+
+
